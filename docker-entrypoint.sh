2f0ed438858d6bc917bd6b47e1b1b5768c7f308c
esme518 <esme518@gmail.com>
2018-04-10 23:09:50 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,15 @@
 
 if [ ! -f UUID ]; then
 	cat /proc/sys/kernel/random/uuid > UUID
-	UUID=$(cat UUID)
 fi
+
+UUID=$(cat UUID)
 
 # Set config.json
 sed -i "s/PORT/$PORT/g" /etc/v2ray/config.json
 sed -i "s/UUID/$UUID/g" /etc/v2ray/config.json
 
-echo starting v2ray server
+echo starting v2ray platform
 echo starting with UUID:$UUID
 echo listening at 0.0.0.0:$PORT
 
